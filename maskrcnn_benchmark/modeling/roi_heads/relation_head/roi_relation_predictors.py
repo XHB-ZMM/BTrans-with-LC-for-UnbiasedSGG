bc39e0a7562bedcde6cb023a8becfa8311cf96a3
XHB <63825035+XHB-ZMM@users.noreply.github.com>
2023-04-10 15:53:33 +0800

    Update roi_relation_predictors.py

--- maskrcnn_benchmark/modeling/roi_heads/relation_head/roi_relation_predictors.py
+++ maskrcnn_benchmark/modeling/roi_heads/relation_head/roi_relation_predictors.py
@@ -14,6 +14,7 @@ from .model_vctree import VCTreeLSTMContext
 from .model_motifs import LSTMContext, FrequencyBias
 from .model_motifs_with_attribute import AttributeLSTMContext
 from .model_transformer import TransformerContext
+from .model_BTrans import BiTransformerContext
 from .utils_relation import layer_init, get_box_info, get_box_pair_info
 from maskrcnn_benchmark.data import get_dataset_statistics
 import pdb
@@ -21,8 +22,8 @@ from .BCLloss import BalSCL
 from operator import add
 from functools import reduce
 
-@registry.ROI_RELATION_PREDICTOR.register("TransformerPredictor")
-class TransformerPredictor(nn.Module):
+@registry.ROI_RELATION_PREDICTOR.register("BiTransformerPredictor")
+class BiTransformerPredictor(nn.Module):
     def __init__(self, config, in_channels):
         super(TransformerPredictor, self).__init__()
         self.attribute_on = config.MODEL.ATTRIBUTE_ON
@@ -43,7 +44,7 @@ class TransformerPredictor(nn.Module):
         assert self.num_att_cls==len(att_classes)
         assert self.num_rel_cls==len(rel_classes)
         # module construct
-        self.context_layer = TransformerContext(config, obj_classes, rel_classes, in_channels)
+        self.context_layer = BiTransformerContext(config, obj_classes, rel_classes, in_channels)
 
         # post decoding
         self.hidden_dim = config.MODEL.ROI_RELATION_HEAD.CONTEXT_HIDDEN_DIM
@@ -161,6 +162,117 @@ class TransformerPredictor(nn.Module):
         else:
             return obj_dists, rel_dists, add_losses
 
+        
+@registry.ROI_RELATION_PREDICTOR.register("TransformerPredictor")
+class TransformerPredictor(nn.Module):
+    def __init__(self, config, in_channels):
+        super(TransformerPredictor, self).__init__()
+        self.attribute_on = config.MODEL.ATTRIBUTE_ON
+        # load parameters
+        self.num_obj_cls = config.MODEL.ROI_BOX_HEAD.NUM_CLASSES
+        self.num_att_cls = config.MODEL.ROI_ATTRIBUTE_HEAD.NUM_ATTRIBUTES
+        self.num_rel_cls = config.MODEL.ROI_RELATION_HEAD.NUM_CLASSES
+
+        assert in_channels is not None
+        num_inputs = in_channels
+        self.use_vision = config.MODEL.ROI_RELATION_HEAD.PREDICT_USE_VISION
+        self.use_bias = config.MODEL.ROI_RELATION_HEAD.PREDICT_USE_BIAS
+
+        # load class dict
+        statistics = get_dataset_statistics(config)
+        obj_classes, rel_classes, att_classes = statistics['obj_classes'], statistics['rel_classes'], statistics['att_classes']
+        assert self.num_obj_cls==len(obj_classes)
+        assert self.num_att_cls==len(att_classes)
+        assert self.num_rel_cls==len(rel_classes)
+        # module construct
+        self.context_layer = TransformerContext(config, obj_classes, rel_classes, in_channels)
+
+        # post decoding
+        self.hidden_dim = config.MODEL.ROI_RELATION_HEAD.CONTEXT_HIDDEN_DIM
+        self.pooling_dim = config.MODEL.ROI_RELATION_HEAD.CONTEXT_POOLING_DIM
+        self.post_emb = nn.Linear(self.hidden_dim, self.hidden_dim * 2)
+        self.post_cat = nn.Linear(self.hidden_dim * 2, self.pooling_dim)
+        self.rel_compress = nn.Linear(self.pooling_dim, self.num_rel_cls)
+        self.ctx_compress = nn.Linear(self.hidden_dim * 2, self.num_rel_cls)
+
+        # initialize layer parameters 
+        layer_init(self.post_emb, 10.0 * (1.0 / self.hidden_dim) ** 0.5, normal=True)
+        layer_init(self.rel_compress, xavier=True)
+        layer_init(self.ctx_compress, xavier=True)
+        layer_init(self.post_cat, xavier=True)
+
+        if self.pooling_dim != config.MODEL.ROI_BOX_HEAD.MLP_HEAD_DIM:
+            self.union_single_not_match = True
+            self.up_dim = nn.Linear(config.MODEL.ROI_BOX_HEAD.MLP_HEAD_DIM, self.pooling_dim)
+            layer_init(self.up_dim, xavier=True)
+        else:
+            self.union_single_not_match = False
+
+        if self.use_bias:
+            # convey statistics into FrequencyBias to avoid loading again
+            self.freq_bias = FrequencyBias(config, statistics)
+
+    def forward(self, proposals, rel_pair_idxs, rel_labels, rel_binarys, roi_features, union_features, logger=None):
+        """
+        Returns:
+            obj_dists (list[Tensor]): logits of object label distribution
+            rel_dists (list[Tensor])
+            rel_pair_idxs (list[Tensor]): (num_rel, 2) index of subject and object
+            union_features (Tensor): (batch_num_rel, context_pooling_dim): visual union feature of each pair
+        """
+        if self.attribute_on:
+            obj_dists, obj_preds, att_dists, edge_ctx = self.context_layer(roi_features, proposals, logger)
+        else:
+            obj_dists, obj_preds, edge_ctx = self.context_layer(roi_features, proposals, logger)
+
+        # post decode
+        edge_rep = self.post_emb(edge_ctx)
+        edge_rep = edge_rep.view(edge_rep.size(0), 2, self.hidden_dim)
+        head_rep = edge_rep[:, 0].contiguous().view(-1, self.hidden_dim)
+        tail_rep = edge_rep[:, 1].contiguous().view(-1, self.hidden_dim)
+
+        num_rels = [r.shape[0] for r in rel_pair_idxs]
+        num_objs = [len(b) for b in proposals]
+        assert len(num_rels) == len(num_objs)
+
+        head_reps = head_rep.split(num_objs, dim=0)
+        tail_reps = tail_rep.split(num_objs, dim=0)
+        obj_preds = obj_preds.split(num_objs, dim=0)
+
+        # from object level feature to pairwise relation level feature
+        prod_reps = []
+        pair_preds = []
+        for pair_idx, head_rep, tail_rep, obj_pred in zip(rel_pair_idxs, head_reps, tail_reps, obj_preds):
+            prod_reps.append(torch.cat((head_rep[pair_idx[:,0]], tail_rep[pair_idx[:,1]]), dim=-1))
+            pair_preds.append(torch.stack((obj_pred[pair_idx[:,0]], obj_pred[pair_idx[:,1]]), dim=1))
+        prod_rep = cat(prod_reps, dim=0)
+        pair_pred = cat(pair_preds, dim=0)
+
+        ctx_gate = self.post_cat(prod_rep)
+
+        # use union box and mask convolution
+        if self.use_vision:
+            if self.union_single_not_match:
+                visual_rep = ctx_gate * self.up_dim(union_features)
+            else:
+                visual_rep = ctx_gate * union_features
+
+        rel_dists = self.rel_compress(visual_rep) + self.ctx_compress(prod_rep)
+
+        # use frequence bias
+        if self.use_bias:
+            rel_dists = rel_dists + self.freq_bias.index_with_labels(pair_pred)
+
+        obj_dists = obj_dists.split(num_objs, dim=0)
+        rel_dists = rel_dists.split(num_rels, dim=0)
+
+        add_losses = {}
+
+        if self.attribute_on:
+            att_dists = att_dists.split(num_objs, dim=0)
+            return (obj_dists, att_dists), rel_dists, add_losses
+        else:
+            return obj_dists, rel_dists, add_losses
 
 @registry.ROI_RELATION_PREDICTOR.register("IMPPredictor")
 class IMPPredictor(nn.Module):
